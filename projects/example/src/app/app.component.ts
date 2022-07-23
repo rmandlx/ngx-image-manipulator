d44167facf94abf6d5a09a3565570ba41ce52c29
Peter Herbst <60853627+richmandlx@users.noreply.github.com>
2022-07-23 14:24:59 +0200

    testing

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as Comlink from 'comlink';
+import { Remote } from 'comlink';
+import { ConcreteImageManipulator } from 'image-manipulator';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example';
+
+  constructor() {}
+
+  async test() {
+    const worker = new Worker(new URL('test.worker', import.meta.url));
+    // WebWorkers use `postMessage` and therefore work with Comlink.
+    const obj = Comlink.wrap(worker) as Remote<ConcreteImageManipulator>;
+    alert(`Counter: ${await obj.counter}`);
+    await obj.inc();
+    alert(`Counter: ${await obj.counter}`);
+  }
 }
